80fb7c9e610af6517fa31410ce3869430e690b4d
Yirong <li.yirong.z25@gmail.com>
2020-01-14 21:09:21 +0000

    add loop

--- src/week1/Main.java
+++ src/week1/Main.java
@@ -8,12 +8,24 @@ public class Main {
 			ArrayList<Cipher> ciphers = new ArrayList();
 			ciphers.add(new Vignere("SPRING"));
 			ciphers.add(new Mono("SPRING"));
-			for(int i = 'A'; i <= 'Z'; i++)
-				System.out.print(ciphers.get(0).encode((char)i));
-			System.out.println();
-			for(int i = 'A'; i <= 'Z'; i++)
-				System.out.print(ciphers.get(1).encode((char)i));
-			System.out.println();
+			
+			String message = "THIS IS A MESSAGE TO ENCODE";
+			String output = "";
+			for(int j = 0; j < 2; j++){
+				for(int i=0;i<message.length();i++) {
+					output += ciphers.get(j).encode(message.charAt(i));
+				}
+				System.out.println(output);
+				output = "";
+			}
+			
+//			for(int i = 'A'; i <= 'Z'; i++)
+//				System.out.print(ciphers.get(0).encode((char)i));
+//			System.out.println();
+//			for(int i = 'A'; i <= 'Z'; i++)
+//				System.out.print(ciphers.get(1).encode((char)i));
+//			System.out.println();
+			
 			ArrayList<CipherString> c2 = new ArrayList();
 			c2.add(new Vignere("WINTER"));
 			c2.add(new Mono("WINTER"));
